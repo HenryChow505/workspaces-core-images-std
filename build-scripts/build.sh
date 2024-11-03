4f800896425b8c6a3adabdfc82e780e8f4d74c9c
HenryChow505 <134348195+HenryChow505@users.noreply.github.com>
2024-11-03 22:00:59 +0800

    ci: 修改 -t 符合命名 kasmcustom

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -10,7 +10,7 @@ DOCKERFILE=$6
 
 ## Build/Push image to cache endpoint by pipeline ID ##
 docker build \
-  -t KasmCustom/core-${NAME1}-${NAME2}:develop \
+  -t kasmcustom/core-${NAME1}-${NAME2}:develop \
   --build-arg BASE_IMAGE="${BASE}" \
   --build-arg DISTRO="${DISTRO}" \
   --build-arg BG_IMG="${BG}" \
